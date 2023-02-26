260e622e68c181d07ff32d905ddebd33ab8caa70
shireeshatheegulla <shireesha.theegulla55@gmail.com>
2023-02-26 11:41:19 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SPFOODS</h1>
+        <h5>Login</h5>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
